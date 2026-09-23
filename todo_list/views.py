1a5bead5404c8c37eb229a1bfffa4ce8d9f44c20
Andre Matthew Balagot <andrebalagot0805@gmail.com>
2026-09-23 11:21:08 +0800

    Initial commit

---
+++ todo_list/views.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from django.shortcuts import render, redirect
+from .models import List
+from .forms import ListForm
+
+
+def home(request):
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+
+    all_items = List.objects.all()
+
+    now = datetime.now()
+    date_str = now.strftime('%B %d %Y')
+
+    context = {
+        'all_items': all_items,
+        'date_today': date_str,
+    }
+
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    context = {
+        'myname': 'Bob'
+    }
+
+    return render(request, 'about.html', context)
+
+
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+
+    return redirect('home')
+
+
+def strike(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = True
+    item.save()
+
+    return redirect('home')
+
+
+def unstrike(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = False
+    item.save()
+
+    return redirect('home')
